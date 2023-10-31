65797764df7946890346cc935de89d8ebc07362f
Ilyas Shagaleev <ilyas1996shagaleev@gmail.com>
2023-10-31 23:19:26 +0500

    Change files structure

--- common/urls.js
+++ common/urls.js
@@ -1,2 +1,1 @@
 export const BASE_URL = 'https://api.nasa.gov/'
-export const APOD_URL = 'apod'
--- src/tests/getAsteroidNeoInfo.js
+++ src/tests/getAsteroidNeoInfo.js
@@ -1,8 +1,6 @@
-import http from 'k6/http';
 import { check, sleep } from 'k6';
-import { BASE_URL } from '../../common/urls.js';
-import { PERSONAL_TOKEN } from '../../common/token.js';
-import { mainOptions } from '../../mainLoadOptions.js';
+import { getInfoAsteroid } from '../../executors/asteroidNeoInfo.js';
+
 
 
 export const options = {
@@ -16,7 +14,7 @@ const endDate = '2022-12-03'
 
 
 export default function () {
-  let response = http.get(`${BASE_URL}neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${PERSONAL_TOKEN}`);
+  let response = getInfoAsteroid(startDate, endDate);
   check(response, {
     'status code is 200': (res) => res.status === 200
   })
